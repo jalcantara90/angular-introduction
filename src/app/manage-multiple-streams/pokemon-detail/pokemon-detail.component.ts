37f87e1b1f3cfc570fcc1f3e0e02f8ba9685101d
Jonathan Alcántara Jiménez <jalcantara1990@gmail.com>
2021-06-06 12:13:52 +0200

    feat: handle-subscriptions

--- src/app/manage-multiple-streams/pokemon-detail/pokemon-detail.component.ts
+++ src/app/manage-multiple-streams/pokemon-detail/pokemon-detail.component.ts
@@ -1,27 +1,37 @@
 import { Pokemon, PokemonService } from 'src/app/shared/pokemon/pokemon.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ConfigurationService } from 'src/app/shared/configuration/configuration.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-detail',
   templateUrl: './pokemon-detail.component.html',
   styleUrls: ['./pokemon-detail.component.scss']
 })
-export class PokemonDetailComponent implements OnInit {
+export class PokemonDetailComponent implements OnInit, OnDestroy {
   pokemon!: Pokemon;
   description!: string;
+  routerSubscription = new Subscription();
+  getPokemonSubscription = new Subscription();
+  getSpiceSubscription = new Subscription();
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private pokemonService: PokemonService
   ) { }
 
+  ngOnDestroy(): void {
+    this.routerSubscription.unsubscribe();
+    this.getPokemonSubscription.unsubscribe();
+    this.getSpiceSubscription.unsubscribe();
+  }
+
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({pokemonId}) => {
-      this.pokemonService.getPokemon(pokemonId).subscribe((pokemon) => {
+    this.routerSubscription = this.activatedRoute.params.subscribe(({pokemonId}) => {
+      this.getPokemonSubscription = this.pokemonService.getPokemon(pokemonId).subscribe((pokemon) => {
         this.pokemon = pokemon;
-        this.pokemonService.getSpice(pokemon.species.url).subscribe(description => this.description = description);
+        this.getSpiceSubscription = this.pokemonService.getSpice(pokemon.species.url).subscribe(description => this.description = description);
       });
     });
   }
